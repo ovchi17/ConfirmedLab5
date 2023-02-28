c155a4718622a29de9896b064f7c31c54c12fa19
jutsoNNN <stas.gonin@mail.ru>
2023-03-01 02:12:50 +0300

    [ADD] command Average of distance

--- src/main/kotlin/workCommandsList/AvarageOfDistance.kt
+++ src/main/kotlin/workCommandsList/AvarageOfDistance.kt
@@ -3,14 +3,31 @@ package workCommandsList
 import controllers.WorkWithCollection
 import dataSet.Route
 import dataSet.RouteComporator
+import usersView.AnswerToUser
+import usersView.ConsoleWriter
 import java.util.*
 
 class AvarageOfDistance: Command {
     override fun execute(str: List<Any>, workWithCollection: WorkWithCollection) {
-        println("aod")
-//        Я тут просто кое-че тестил, пока не пустое)))
+
+        val consoleWriter: ConsoleWriter = ConsoleWriter()
         val collection = PriorityQueue<Route>(RouteComporator())
         collection.addAll(workWithCollection.getCollection())
-        println(collection.toArray())
+
+        val answerToUser: AnswerToUser = AnswerToUser()
+
+        var sizeCollection = collection.toList()
+
+        if (collection.size == 0){
+            consoleWriter.printToConsoleLn("emptyCollection")
+        }else{
+            var distances: Long = 0
+            var result: Double
+            for(i in 0..(sizeCollection.count()-1)){
+                distances += sizeCollection.get(i).distance
+            }
+            result = distances / sizeCollection.count().toDouble()
+            answerToUser.writeToConsoleLn(result.toString())
+        }
     }
 }
